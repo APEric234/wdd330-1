65eaac49c4c8c8b1488a08cf01a5dc988d4f6e3b
Eric Allen <aperic234@gmail.com>
2021-01-23 15:19:03 -0700

    fixed homepage to be generated instead of typed

---
+++ weekloading.js
@@ -0,0 +1,16 @@
+const work_details = [
+    { week: "1",work: "index.html" },
+    { week: "2",work: "index.html" },
+    { week: "3",work: "index.html" },
+  ];
+  var ol_links = document.getElementById("links");
+  for(week in work_details){
+      week=work_details[week];
+      var li =document.createElement("li");
+      var link = document.createElement("a");
+      link.innerHTML = "week " +week["week"];
+      link.href = "portfolio/week"+week["week"]+"/"+week["work"];
+      li.appendChild(link);
+      ol_links.appendChild(li);
+      
+  }
