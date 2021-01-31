136b85e642bd8e4e0601ae46cf3b93cd0dca8fb3
Eric Allen <aperic234@gmail.com>
2021-01-30 22:53:50 -0700

    fixed

--- weekloading.js
+++ weekloading.js
@@ -2,6 +2,8 @@ const work_details = [
     { week: "1",work: "index.html" },
     { week: "2",work: "index.html" },
     { week: "3",work: "index.html" },
+    { week: "4",work: "hero.html" },
+    { week: "4",work: "teamactivity.html" },
   ];
   var ol_links = document.getElementById("links");
   for(week in work_details){
