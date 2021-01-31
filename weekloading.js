2a51b5098b988225f011ff2485e47968a081f546
Eric Allen <aperic234@gmail.com>
2021-01-30 23:30:23 -0700

    added latest file

--- weekloading.js
+++ weekloading.js
@@ -1,18 +1,18 @@
 const work_details = [
-    { week: "1",work: "index.html" },
-    { week: "2",work: "index.html" },
-    { week: "3",work: "index.html" },
-    { week: "4",work: "hero.html" },
-    { week: "4",work: "teamactivity.html" },
+    { week: "1",Message:"week 1",work: "index.html" },
+    { week: "2",Message:"week 2",work: "index.html" },
+    { week: "3",Message:"week 3",work: "index.html" },
+    { week: "4",Message:"week4: hero with options",work: "hero.html" },
+    { week: "4",Message:"week4: team activity",work: "teamactivity.html" },
 
-    { week: "4/npm/dist",work: "index.html" },
+    { week: "4/npm/dist",Message:"week4: npm",work: "index.html" },
   ];
   var ol_links = document.getElementById("links");
   for(week in work_details){
       week=work_details[week];
       var li =document.createElement("li");
       var link = document.createElement("a");
-      link.innerHTML = "week " +week["week"];
+      link.innerHTML = week["Message"];
       link.href = "portfolio/week"+week["week"]+"/"+week["work"];
       li.appendChild(link);
       ol_links.appendChild(li);
