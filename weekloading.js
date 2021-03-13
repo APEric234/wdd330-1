9cf79b0c7d7a18880375ce6b42c71cef8341fe1f
Eric Allen <aperic234@gmail.com>
2021-03-13 13:44:13 -0700

    more sensibly named variables

--- weekloading.js
+++ weekloading.js
@@ -1,15 +1,17 @@
 const work_details = [
-    { week: "1",Message:"week 1",work: "index.html" },
-    { week: "2",Message:"week 2",work: "index.html" },
-    { week: "3",Message:"week 3",work: "index.html" },
-    { week: "4",Message:"week4: hero with options",work: "hero.html" },
-    { week: "4",Message:"week4: team activity",work: "teamactivity.html" },
+    { week: "1",Message:"week 1",file: "index.html" },
+    { week: "2",Message:"week 2",file: "index.html" },
+    { week: "3",Message:"week 3",file: "index.html" },
+    { week: "4",Message:"week4: hero with options",file: "hero.html" },
+    { week: "4",Message:"week4: team activity",file: "teamactivity.html" },
 
-    { week: "4/npm/dist",Message:"week4: npm",work: "index.html" },
-    { week: "5",Message:"week5: hero with logging",work: "hero.html" },
+    { week: "4/npm/dist",Message:"week4: npm",file: "index.html" },
+    { week: "5",Message:"week5: hero with logging",file: "hero.html" },
     
-    { week: "7",Message:"week7: hero with random",work: "ninja.html" },
-    { week: "8",Message:"week8: transform example",work: "base.html" },
+    { week: "7",Message:"week7: hero with random",file: "ninja.html" },
+    { week: "8",Message:"week8: transform example",file: "base.html" },
+    { week: "10",Message:"week10: notes on fetch and client side validation",file: "index.html" },
+
 
 ];
   var ol_links = document.getElementById("links");
@@ -18,7 +20,7 @@ const work_details = [
       var li =document.createElement("li");
       var link = document.createElement("a");
       link.innerHTML = week["Message"];
-      link.href = "portfolio/week"+week["week"]+"/"+week["work"];
+      link.href = "portfolio/week"+week["week"]+"/"+week["file"];
       li.appendChild(link);
       ol_links.appendChild(li);
       
